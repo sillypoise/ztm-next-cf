1a2b4796ef5ad2727b0b549096a9826f235ca34a
sillypoise <somewhatjose@gmail.com>
2023-02-06 13:31:44 -0500

    main: still styling it out!

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "@next/font/google";
 import { Forms } from "~/components/forms";
+import { Code } from "~/components/code";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,6 +23,7 @@ export default function Home() {
             <main>
                 <article className="center stack mlb-l">
                     <h2>Coffee Finder</h2>
+                    <Code/>
                     <Forms />
                     <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ea, expedita quae totam deserunt sed optio quos dolore natus praesentium hic iusto? Voluptas beatae natus nesciunt accusantium cum libero ad eum porro similique architecto maiores eligendi voluptates, neque rerum repudiandae aliquid ducimus aperiam, qui ipsa dolor commodi recusandae, voluptatum necessitatibus. Ipsum nobis nisi deserunt voluptatum fuga sapiente, dolorum ducimus ab labore iste nemo, quam, dolore perferendis saepe. Labore pariatur esse voluptas? Omnis officia molestias a commodi explicabo accusantium veritatis voluptate placeat, fugit autem repellat aspernatur iste quidem molestiae, ad praesentium perspiciatis, necessitatibus aliquam non eum cupiditate deserunt. Voluptas hic facilis perferendis blanditiis deserunt earum incidunt? Sit aspernatur quam aliquam ea deleniti ad fuga, sunt corrupti labore maiores esse quis assumenda recusandae consectetur, voluptate natus, provident libero neque ipsum iste perferendis! In, maxime sed, labore cum sint enim modi eos, dignissimos vel nisi fugiat! Libero iusto enim est, possimus inventore id temporibus.</p>
                 </article>
