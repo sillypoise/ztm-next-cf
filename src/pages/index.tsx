79469278c43271293d23e4acae5cbccee6e4c378
sillypoise <somewhatjose@gmail.com>
2023-02-06 12:35:37 -0500

    main: really styling this one out I suppose

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,9 +20,10 @@ export default function Home() {
                 />
             </Head>
             <main>
-                            <article className="center stack mlb-l">
+                <article className="center stack mlb-l">
                     <h2>Coffee Finder</h2>
                     <Forms />
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ea, expedita quae totam deserunt sed optio quos dolore natus praesentium hic iusto? Voluptas beatae natus nesciunt accusantium cum libero ad eum porro similique architecto maiores eligendi voluptates, neque rerum repudiandae aliquid ducimus aperiam, qui ipsa dolor commodi recusandae, voluptatum necessitatibus. Ipsum nobis nisi deserunt voluptatum fuga sapiente, dolorum ducimus ab labore iste nemo, quam, dolore perferendis saepe. Labore pariatur esse voluptas? Omnis officia molestias a commodi explicabo accusantium veritatis voluptate placeat, fugit autem repellat aspernatur iste quidem molestiae, ad praesentium perspiciatis, necessitatibus aliquam non eum cupiditate deserunt. Voluptas hic facilis perferendis blanditiis deserunt earum incidunt? Sit aspernatur quam aliquam ea deleniti ad fuga, sunt corrupti labore maiores esse quis assumenda recusandae consectetur, voluptate natus, provident libero neque ipsum iste perferendis! In, maxime sed, labore cum sint enim modi eos, dignissimos vel nisi fugiat! Libero iusto enim est, possimus inventore id temporibus.</p>
                 </article>
             </main>
         </>
