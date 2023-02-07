12f3e59e01e9b0d37a872c2ebf15fcf3a0faa557
sillypoise <somewhatjose@gmail.com>
2023-02-06 22:11:23 -0500

    main: keep on styling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -27,35 +27,8 @@ export default function Home() {
             <main>
                 <article className="center stack mlb-l">
                     <h2>Coffee Finder</h2>
-                    <div className="box bg-[var(--overlay-black-10)]">HELLO</div>
-                    <div>
-                        {" "}
-                        <button type="button" id="dialog-trigger" 
-                            onClick={() => console.log(dialogRef.current.showModal())}
-                        >
-                            {" "}
-                            Show me the dialog!{" "}
-                        </button>{" "}
-                        <span id="dialog-result"></span>{" "}
-                    </div>
-                    <dialog ref={dialogRef} id="dialog">
-                        {" "}
-                        <header>This is a sample dialog</header>{" "}
-                        <form method="dialog">
-                            {" "}
-                            <p>What is your favorite pet animal?</p>{" "}
-                            <menu>
-                                {" "}
-                                <button value="feline">Cats</button>{" "}
-                                <button value="canine">Dogs</button>{" "}
-                                <button value="other">Others</button>{" "}
-                            </menu>{" "}
-                        </form>
-                    </dialog>
                     <Overview />
-                    {/* <Code/> */}
-                    {/* <Table /> */}
-                    {/* <Forms /> */}
+                    <Forms />
                 </article>
             </main>
         </>
