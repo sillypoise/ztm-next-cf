af2d9631e53c6760a026937e29523b0d6b8780cc
sillypoise <somewhatjose@gmail.com>
2023-02-22 14:10:07 -0500

    Add mock coffee store data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import { Inter } from "@next/font/google";
 import { useRef } from "react";
 import { CoffeStoreCard } from "~/components/CoffeeCard";
+import { coffeeStoresMockData } from "~/test/mocks/coffeeStoresData"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -27,10 +28,10 @@ export default function Home() {
                     <p>Discover your new favourite coffee shop</p>
                     <button>View stores nearby</button>
                     <section className="auto-grid gap-2xs" data-layout="2/2" data-rows="masonry">
-                        {Array(20).fill(0).map((_, i) => (
+                        {coffeeStoresMockData.map((store, i) => (
                             <CoffeStoreCard
-                                key={i}
-                                title="Early Riser"
+                                key={store.id}
+                                title={store.name}
                                 adress="1234 Coffee Street, Coffee City, Coffee Country"
                                 imgURL="https://unsplash.com/photos/3b2tADGAWnU/download?ixid=MnwxMjA3fDB8MXxzZWFyY2h8OHx8Y29mZmVlJTIwc2hvcHxlbnwwfHx8fDE2NzcwNTg1NTU&force=true&w=1920"
                                 imgAlt="generic coffee store"
---
+++ src/test/mocks/coffeeStoresData.ts
@@ -0,0 +1,82 @@
+let coffeeStoresMockData = [
+    {
+        "id": 1,
+        "name": "The Daily Grind",
+        "address": "123 Main St",
+        "image": "https://images.unsplash.com/photo-1565299623005-dce1e3b3bd6f"
+    },
+    {
+        "id": 2,
+        "name": "Coffee House",
+        "address": "456 First St",
+        "image": "https://images.unsplash.com/photo-1572318493908-1a31d7a48fae"
+    },
+    {
+        "id": 3,
+        "name": "Java Junction",
+        "address": "789 Second St",
+        "image": "https://images.unsplash.com/photo-1541439476222-9a68d30e2141"
+    },
+    {
+        "id": 4,
+        "name": "Mocha Madness",
+        "address": "321 Third St",
+        "image": "https://images.unsplash.com/photo-1600971568482-78a276f9a5fd"
+    },
+    {
+        "id": 5,
+        "name": "Brewed Awakening",
+        "address": "654 Fourth St",
+        "image": "https://images.unsplash.com/photo-1584029656551-4a07026e72e1"
+    },
+    {
+        "id": 6,
+        "name": "Perk Up Cafe",
+        "address": "987 Fifth St",
+        "image": "https://images.unsplash.com/photo-1512944137431-6d99cf6f76f6"
+    },
+    {
+        "id": 7,
+        "name": "Roasted",
+        "address": "246 Sixth St",
+        "image": "https://images.unsplash.com/photo-1492619379795-5b5a46eacf84"
+    },
+    {
+        "id": 8,
+        "name": "Bean Scene",
+        "address": "369 Seventh St",
+        "image": "https://images.unsplash.com/photo-1519681393784-d120267933ba"
+    },
+    {
+        "id": 9,
+        "name": "Grind House",
+        "address": "753 Eighth St",
+        "image": "https://images.unsplash.com/photo-1526827550524-4cfc72daa9b4"
+    },
+    {
+        "id": 10,
+        "name": "Espresso Yourself",
+        "address": "159 Ninth St",
+        "image": "https://images.unsplash.com/photo-1532001473186-1e1cb91605d5"
+    },
+    {
+        "id": 11,
+        "name": "Black Gold",
+        "address": "357 Tenth St",
+        "image": "https://images.unsplash.com/photo-1544717296-1b4041eefc4a"
+    },
+    {
+        "id": 12,
+        "name": "Sip and Savor",
+        "address": "456 Eleventh St",
+        "image": "https://images.unsplash.com/photo-1512486130939-4c71b7f0eadd"
+    },
+    {
+        "id": 13,
+        "name": "Daily Grind Cafe",
+        "address": "654 Twelfth St",
+        "image": "https://images.unsplash.com/photo-1564069110239-12f9ac9d18b8"
+    }
+]
+
+export { coffeeStoresMockData }
